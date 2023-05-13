0eb6a178b6ffbce1eccc1917a4abd6a07de6f06f
Manjunathan-16 <manjunathan.16.1998@gmail.com>
2023-05-13 18:51:59 +0530

    Array List practice

--- src/HashMapPractice.java
+++ src/HashMapPractice.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class HashMapPractice {
@@ -6,14 +7,22 @@ public class HashMapPractice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		HashMap<Integer, String> map = new HashMap<Integer, String>();
-		map.put(1, "M");
-		map.put(2, "a");
-		map.put(3, "n");
-		map.put(4, "j");
-		for (Map.Entry m : map.entrySet())
-			System.out.println(m.getKey() + " " + m.getValue());
-
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("Name", "Manjunath");
+		map.put("Age", "25");
+		map.put("Place", "Bangalore");
+		map.put("Sex", "Male");
+		// printing hashmap using for each loop
+		for (Map.Entry m : map.entrySet()) {
+			System.out.println("Printing from for each loop : " + m.getKey() + " " + m.getValue());
+		}
+		System.out.println("\n");
+		// printing hashmap using set and iterator
+		Iterator itr = map.entrySet().iterator();
+		while (itr.hasNext()) {
+			Map.Entry mi = (Map.Entry) itr.next();
+			System.out.println("Displaying through iterator : " + mi.getKey() + " " + mi.getValue());
+		}
 	}
 
 }

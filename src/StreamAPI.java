bb996623a1cb80268738c0ef5cf082e51ac3c403
Manjunathan-16 <manjunathan.16.1998@gmail.com>
2023-08-21 10:40:03 +0530

    edited streams

--- src/StreamAPI.java
+++ src/StreamAPI.java
@@ -8,19 +8,20 @@ public class StreamAPI {
 		// TODO Auto-generated method stub
 		List<Integer> val = Arrays.asList(6, 2, 7, 4, 5, 1);
 		// By using stream in a single line function
-		val.stream()
-		.filter(n -> n % 2 == 1) // filtering with odd values
-		.sorted() // sorting
-		.map(n -> n * 2) // performing task
-		.forEach(n -> System.out.println(n)); // printing the output
+		val.stream().filter(n -> n % 2 == 1) // filtering with odd values
+				.sorted() // sorting
+				.map(n -> n * 2) // performing task
+				.forEach(n -> System.out.println(n)); // printing the output
 
-		// This is by assigning each step to individual variable 
-//		Stream<Integer> s = val.stream();
-//		Stream<Integer> p = s.filter(n->n%2==1);
-//		Stream<Integer> m = p.sorted();
-//		Stream<Integer> o = m.map(a -> a * 2);
-//		o.forEach(n -> System.out.println(n));
-//		int r = o.reduce( 0,(c,e)->c+e);
+		// The code is same as above just by assigning each step to individual variable
+		Stream<Integer> s = val.stream();
+		Stream<Integer> p = s.filter(n -> n % 2 == 1);
+		Stream<Integer> m = p.sorted();
+		Stream<Integer> o = m.map(a -> a * 2);
+		o.forEach(n -> System.out.println(n));
+
+		// reducing into single value and displaying
+//		int r = o.reduce( 0,(c,e)->c+e); // adding all values
 //		System.out.println(r);
 	}
 }

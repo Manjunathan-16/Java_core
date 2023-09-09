d7623b454d1e25ea97779d61ccec7167368908ab
Manjunathan-16 <manjunathan.16.1998@gmail.com>
2023-09-09 17:10:31 +0530

    Convert zeros to five in digit

--- src/DailyCoding/ConvertFive.java
+++ src/DailyCoding/ConvertFive.java
@@ -4,15 +4,20 @@ public class ConvertFive {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int n = 1005;
-while(n!=0) {
-	int digit =n%10;
-	if(digit == 0) {
-		digit = 5;
-	}
-	System.out.println(digit);
-	break;
-}
+		int n = 12106;
+		int digit = 0;
+		int power = 1;
+		int temp = 0;
+		while (n != 0) {
+			digit = n % 10;
+			if (digit == 0) {
+				digit = 5;
+			}
+			temp += digit * power;
+			n = n / 10;
+			power = power * 10;		
+		}
+		System.out.println(temp);
 
 	}
 
